05fd7cd1947de4e0157a0b6d09d70f2b640c0231
kouki-tsuchiya <74812031+kouki-tsuchiya@users.noreply.github.com>
2020-11-21 17:38:46 +0900

    Update keijiban.php

--- keijiban.php
+++ keijiban.php
@@ -61,9 +61,9 @@
 
 <?php
 //データベースに接続
-    $dsn = データベース名;
-	$user = ユーザー名;
-	$password = パスワード;
+    　　$dsn = **********;
+	$user = **********;
+	$password = *********;
 	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	
 
